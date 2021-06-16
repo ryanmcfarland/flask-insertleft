e515746dbf2a5f4ae2d946fed0a4c8626ee92fdf
ryanmcfarland@outlook.com <ryanmcfarland@outlook.com>
2021-06-16 20:35:38 +0100

    adding lower_case check

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,19 +4,20 @@ from app.models import User
 
 
 class LoginForm(Form):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=30)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegistrationForm(Form):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=128)])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password', message='Please enter matching passwords.')])
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        username=username.data.lower()
+        user = User.query.filter(User.lowercase_username==username).first()
         if user is not None:
             raise ValidationError('Username is already in use.')
 
