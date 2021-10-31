9d1d20942038bc9ad9c9e7aaaa4eb0a83210b32e
ryanmcfarland@outlook.com <ryanmcfarland@outlook.com>
2021-10-31 17:25:58 +0000

    added missing columns

--- app/beatcops/__init__.py
+++ app/beatcops/__init__.py
@@ -1,10 +1,9 @@
 class Config(object):
     attributes = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma" ]
     classes=["Brain", "Brawn"]
-    backgrounds=["Traffic Branch", "Armed Response Unit", 
-                "Dog Section", "Detective", "Tactical Support Group", "Cyber Crime", 
-                "Response", "Neighbourhood", "PACE Officer", "Counter Terror Firearms Officer" ]
-    skills = [ "administer", "animal_handling", "connect", "drive", "fix", "heal", "investigate", 
+    backgrounds=["Traffic Branch", "Armed Response Unit", "Dog Section", "Detective", "Tactical Support Group", 
+                "Cyber Crime", "Response", "Neighbourhood", "PACE Officer", "Covert Policing Unit", "Counter Terror Firearms Officer" ]
+    skills = [ "administer", "animal_handling", "connect", "drive", "exert", "fix", "heal", "investigate", 
             "know", "lead", "notice", "perform", "program", "punch", "program", "requisition", "search",
             "shoot",  "stealth",  "strike", "survive", "talk", "work" ]
 
